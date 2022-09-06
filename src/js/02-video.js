55372c498de44802f52651c5defd0973d5bc1d38
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-09-06 23:22:01 +0300

    add 2 task and start doing 3 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+player.on('timeupdate', throttle(updateTime, 1000));
+
+function updateTime(param) {
+  localStorage.setItem('videoplayer-current-time', param.seconds);
+}
+
+const getDataStorage = localStorage.getItem('videoplayer-current-time');
+
+if (getDataStorage) {
+  player.setCurrentTime(getDataStorage);
+}
+
+player.setColor('#FF0000');
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const formRefs = document.querySelector('.feedback-form');
+
+formRefs.addEventListener('submit', onFormSubmit);
+formRefs.addEventListener('input', onTextareaInput);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.dir(event.currenTarget);
+}
+
+function onTextareaInput(event) {}
+
+// ТОЛЬКО ДЛЯ ТЕКСТАРИИ
+// const refs = {
+//   form: document.querySelector('.feedback-form'),
+//   textarea: document.querySelector('[name="message"]'),
+// };
+// const STORAGE_KEY = 'feedback-form-state';
+// populateTextarea();
+
+// refs.form.addEventListener('submit', onFormSubmit);
+// refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+
+//   event.currentTarget.reset();
+//   localStorage.removeItem(STORAGE_KEY);
+// }
+
+// function onTextareaInput(event) {
+//   const message = event.target.value;
+//   localStorage.setItem(STORAGE_KEY, message);
+// }
+
+// function populateTextarea() {
+//   const savedMessage = localStorage.getItem(STORAGE_KEY);
+//   if (savedMessage) {
+//     refs.textarea.value = savedMessage;
+//   }
+// }
